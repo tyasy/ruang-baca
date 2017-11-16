506dcac2d920a5a681a31269991c471f8ae137f9
tyasy <tys.yuni@gmail.com>
2017-11-16 23:25:04 +0700

    outputjournal
    menampilkan hasil inputan journal

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	session_start();
-	include("../../config.php"); 
+	include("../../config.php");
+
+	$queryJournal = mysqli_query($db,"SELECT b.username, b.judul_jurnal, b.tulisan as p WHERE b.username = p.username");
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,82 +17,74 @@
     <script type="text/javascript" src="../../js/jquery.js"></script>
 	<script type="text/javascript" src="../../js/bootstrap.js"></script>
 	<script type="text/javascript" src="../../js/jquery-ui.js"></script>
-	<script type="text/javascript">
-		var rootURL;
-		var prevURL;
-		var currentURL;
-		function getURLS(){
-			rootURL = "http://" + window.location.hostname + document.getElementById("ROOT-URL").innerHTML;
-			prevURL = document.referrer;
-			currentURL = window.location.href;
-		}
-
-		function validateInputjournal(){
-			getURLS();
-			var username = document.forms["jurnal-form"]["user-name"].value;
-			var juduljurnal = document.forms["jurnal-form"]["judul-jurnal"].value;
-			var tulisan = document.forms["jurnal-form"]["tulisan"].value;
-			if(username == "" || juduljurnal == "" || tulisan == ""){
-				document.getElementById("warning-label").innerHTML = "Semua kolom harus diisi";
-				document.getElementById("warning-label").style.display = "";
-			} else {
-					document.getElementById("warning-label").style.display = "none";
-					$.ajax({
-	            		dataType: 'html',
-			            url:'../../ajax/registerRequest.php',
-			            method:'post',
-			            data : {'username':username,'juduljurnal':juduljurnal,'tulisan':tulisan},
-			            success:function(response){
-			            	if(response == "y"){
-			            		alert("Berhasil menginput jurnal");
-			            		window.location = rootURL+"/p/login/";
-			            	} else {
-			            		alert(response);
-			            	}
-			            }
-			        });
-				}
-			}	
-	</script>
 </head>
 <body>
-<label onload="getURLS()" id="ROOT-URL" style="display: none"><?php echo ROOT_URL; ?></label>
+<div class="backgroundHeader">
+</div>
 <div class="body">
-	<div class="container" style="height: fit-content;">
-		<div style="text-align: center;" >
-			<img src="../../images/logo2.PNG" width="300">
+	<?php include_once(ROOT_DIR . "/header.php"); ?>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavbar">
+					<span class="icon-bar"></span>
+	        		<span class="icon-bar"></span>
+	        		<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse" id="mainNavbar">
+				<ul class="nav navbar-nav">
+			    	<li><a href="<?php echo ROOT_URL;?>">Home</a></li>
+			    	<li><a href="<?php echo ROOT_URL;?>">Catalog</a></li>
+			    	<li><a href="<?php echo ROOT_URL . '/p/quotes';?>">Quotes</a></li>
+			    	<li class="active"><a href="<?php echo ROOT_URL . '/p/outputjournal';?>">Reading Journal</a></li>
+			    	<li><a href="#">RuBa Community</a></li>
+			    	<li><a href="<?php echo ROOT_URL . '/p/faq';?>">FAQ</a></li>
+				</ul>
+				<form class="navbar-form navbar-right">
+				    <div class="input-group">
+				    	<input type="text" class="form-control" placeholder="Search">
+				    	<div class="input-group-btn">
+				    		<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+				    	</div>
+				    </div>
+		    	</form>
+			</div>
 		</div>
-		<div class="loginregisterContainer">
-			<p>Isi Jurnal Membaca</p>
-			<form class="readingjurnal-form" id="jurnal-form">
-				<fieldset id="register">
-					<ul>
-						<label>Username</label>
-						<li>
-							<input type="text" name="user-name">
-						</li>
-						<label>Judul Buku</label>
-						<li>
-							<input type="text" name="judul-jurnal">
-						</li>
-						<label>Pengarang</label>
-						<li>
-							<input type="text" name="pengarang">
-						</li>						
-						<label>Jurnal/Review</label>
-						<li>
-							<textarea type="text" name="tulisan" rows="5" ></textarea>
-						</li>
-						<br>
-						<label id="warning-label" style="display: none; color: red; font-size: 0.9em;"></label>
-						<button type="button" class="btn inputjurnal-btn" onclick="validateInputjournal()">Submit</button>
-					</ul>
-				</fieldset>
-			</form>
-			<a href="<?php echo ROOT_URL;?>" style="width: fit-content; display: inline-block;"><span class="glyphicon glyphicon-chevron-left"></span>Kembali</a>
-			<a href="<?php echo ROOT_URL . '/p/outputjournal';?>" style="width: fit-content; display: inline-block; float: right;">Lihat Journal Anda</a>
+	</nav>
+	<div class="mainpage row" style="padding: 0px 50px">
+		<div class="col-md-12" style="margin-bottom: 60px; height: -webkit-fill-available">
+			<div class="sectionTitle">
+				<label>Reading Journal</label>
+			</div>
+			<div class="journalSearch">
+				<div class="input-group">
+				    <input type="text" class="form-control" placeholder="Search Journal">
+				    <div class="input-group-btn">
+				    	<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+				    </div>
+				</div>
+			</div>
+			<div class="journalWrapper grid-item">
+				<ul> 
+					<li>
+						<div class="displayJournal">
+							<div class="Journal-detail">
+								<div class="Journal-username">Oleh <span href="<?php echo $data['username'];?></span></div> 
+								<div class="Journal-juduljurnal"><span href="<?php echo '../outputjournal/index.php?id='.$idjurnal; ?>"><?php echo $data['juduljurnal'];?></span></div>
+								<div class="Journal-tulisan"><span href="<?php echo '../outputjournal/index.php?id='.$idjurnal; ?>"><?php echo $data['tulisan'];?></span></div>
+								</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="tambahJournal" style="float: right; padding-top: 10px; padding-right: 10px;">
+			<a class="btn" class="glyphicon glyphicon-plus" href="<?php echo ROOT_URL . '/p/inputjournal';?>" style="width: fit-content; display: inline-block; float: right;">Add Your Journal</a>
+			</div>
 		</div>
 	</div>
+</div>
+<div class="footer">
 </div>
 </body>
 </html>
